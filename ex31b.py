4a423c68289c890988141798fc7aaebae31d4f53
Gary <gary@Garys-MacBook-Air.local>
2015-10-07 11:55:13 +1100

    ex31b.py

---
+++ ex31b.py
@@ -0,0 +1,21 @@
+print "The Game of Life. Are you ready?"
+
+ready = raw_input("Ready or not (y/n)")
+
+if ready == "y":
+	print "Great, you must go to the bar and order a drink. What you gunna get punk? Hefe, Lager or Coke?"
+	
+	order = raw_input("> ")
+	
+	if order == "Python":
+		print "Good man, you know quality!"
+	elif order == "Ruby" :
+		print "Well, not the best beer but at least you're not a poof."
+	else:
+		print "Faggot!"
+
+elif ready == "n":
+	print "You need to go have a chat with yourself!"
+
+else:
+	print "Fuck you bitch, you didn't answer yay or nay. You must comply with my will or die!"
